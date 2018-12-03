6ec2d59047c6227673c0ec6ba041ebb1cc2082da
Brandon Lyons <lyonsbp@umich.edu>
2018-12-03 15:43:01 -0500

    Added NotFound.vue component

---
+++ components/NotFound.vue
@@ -0,0 +1,11 @@
+<template>
+  <v-card 
+    dark
+    color="primary"
+  >
+    <v-card-title class="title">Page Not Found</v-card-title>
+    <v-card-text class="text-sm-left">
+      The requested page could not be found.
+    </v-card-text>
+  </v-card>
+</template>
