9ed1fd91a6c4af076b05883b6a8e47f5ae6c6b69
Brandon Lyons <lyonsbp@umich.edu>
2019-04-11 15:38:06 -0400

    Added sizing helpers to SvgIcon component

--- components/SvgIcon.vue
+++ components/SvgIcon.vue
@@ -1,11 +1,24 @@
 <template>
-    <div v-if="svg" v-html="svg" :style="cssProps"></div>
+  <div v-if="svg" v-html="svg" :style="cssProps" :class="size"></div>
 </template>
 
 <script>
 import axios from "axios"
 export default {
-  props: ["color", "url"],
+  props: {
+    color: {
+      type: String,
+      default: '#00274c'
+    },
+    url: {
+      type: String,
+      required: true
+    },
+    size: {
+      type: String,
+      default: 'md'
+    }
+  },
   data () {
     return {
       svg: null,
@@ -27,8 +40,21 @@ export default {
 }
 </script>
 
-<style scoped>
+<style scoped lang="scss">
 div {
   color: var(--fill-color);
+
+  .sm {
+    width: 1em;
+  }
+  .md {
+    width: 1.5em;
+  }
+  .lg {
+    width: 3em;
+  }
+  .xl {
+    width: 4em;
+  }
 }
 </style>
