caa39c3027162c0e228b6a0a2977f1fcbd20d0d0
dwreck <6319176+dwreck@users.noreply.github.com>
2023-08-26 10:33:39 -0400

    font weights now correct

--- components/McityToolbarVuetify1.5.vue
+++ components/McityToolbarVuetify1.5.vue
@@ -52,7 +52,7 @@
                       :class="getForMembersClass(itemCat, hover)"
                       >
                       <svg-icon :url="itemCat.svg" :style="getFontColorForMembers(itemCat.text)" />
-                      <v-list-tile-content class="mcity-menu-category-member" :style="getFontColorForMembers(itemCat)" style="overflow:visible; font-size: 20px; white-space: nowrap;">
+                      <v-list-tile-content class="mcity-menu-category-member" :style="getFontColorForMembers(itemCat)" style="overflow:visible; font-size: 20px; font-weight: 500; white-space: nowrap;">
                         {{itemCat.toUpperCase()}}
                       </v-list-tile-content>
                     </v-list-tile>
@@ -74,7 +74,7 @@
                     >
                       <div class="d-flex" style="padding-left:66px;">
                         <svg-icon :url="item.svg"/>
-                        <v-list-tile-content class="mcity-menu-category-member" color="primary--text" style="overflow:visible; font-size: 18px; white-space: nowrap;">
+                        <v-list-tile-content class="mcity-menu-category-member" color="primary--text" style="overflow:visible; font-size: 18px; font-weight: 300; white-space: nowrap;">
                           {{ item.text }}
                         </v-list-tile-content>
                       </div>
